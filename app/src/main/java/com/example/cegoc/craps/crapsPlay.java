e1f8c0283b93da8c04cd9271b0a3893a2775f6e7
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2017-11-16 23:26:46 +0100

    Añadidos_2
    -Creados arrays de dados de distinto color
    -Añadido dados y boton a la interfaz

--- app/src/main/java/com/example/cegoc/craps/crapsPlay.java
+++ app/src/main/java/com/example/cegoc/craps/crapsPlay.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 
 public class crapsPlay extends AppCompatActivity {
 
+    private int dado1,dado2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.craps_play);
+    }
+
+    /**
+     * Se simula que se tiran 2 dados
+     * @return
+     */
+    private int tirarDados(){
+
+
+        return -1;
     }
 }
