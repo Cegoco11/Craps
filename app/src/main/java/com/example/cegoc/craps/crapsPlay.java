5eb87e7fda363fb35b8399adfa49935c9f00a5d5
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2017-11-17 20:05:08 +0100

    Alpha
    +Añadidas las siguientes funciones a crapsPlay.java
    -playCraps(): gestiona todo el juego
    -tirarDados(): se ha editado haciendo que el valor de los dados sea el real
    -primeraRonda(): este metodo simula la primera ronda del juego, con un 7 o 11 se gana, con un 2,3 o 12 se pierde (en ambos casos se retorna un 0), si no, se retorna el valor total de los dados
    -rondas(): este metodo simularia la segunda ronda y las consecutivas en caso de que se den. Si se saca un 7 se pierde, si se saca el mismo numero que en la ronda 1 se gana
    -estadoInicial(): este metodo auxiliar sirve para resear al valor inicial a las variables necesarias para darse otra partida
    
    +Las monedas ganadas o perdidas se almacenan en las variables finales:
    MONEDAS_GANADAS
    MONEDAS_PERDIDAS
    
    +Añadido texto que muestra el numero que se saca en la tirada 1

--- app/src/main/java/com/example/cegoc/craps/crapsPlay.java
+++ app/src/main/java/com/example/cegoc/craps/crapsPlay.java
@@ -6,53 +6,76 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class crapsPlay extends AppCompatActivity {
 
+    private final int MONEDAS_GANADAS=10;
+    private final int MONEDAS_PERDIDAS=8;
     private String arrDado[];
     private ImageView img1;
     private ImageView img2;
     private LinearLayout dadosLayout;
-    private boolean control;
-    private int dado1, dado2;
+    private TextView tiradaText;
+    private boolean control,hasJugado;
+    private int dado1, dado2, valorTirada1, monedas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.craps_play);
 
-        control=false;
+        // ToDo comprobar en save las monedas
+        monedas=0;
+
+        estadoInicial();
+
         arrDado=getResources().getStringArray(R.array.dadosGris);
         img1=(ImageView) findViewById(R.id.dado1);
         img2=(ImageView) findViewById(R.id.dado2);
         dadosLayout=(LinearLayout)findViewById(R.id.dados);
+        tiradaText=(TextView)findViewById(R.id.tiradaRef);
 
         dadosLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                control=!control;
-                tirarDados();
+                playCraps();
             }
         });
     }
 
     /**
-     * Se simula que se tiran 2 dados
-     * @return
+     * Metodo que gestiona el juego
+     */
+    private void playCraps(){
+        control=!control;
+        if(!hasJugado){
+            valorTirada1=primeraRonda();
+            tiradaText.setText(valorTirada1);
+            hasJugado=true;
+        } else{
+            rondas();
+        }
+    }
+
+    /**
+     * Mientras control sea 'true' los dados se mueven
+     * Cuando control pasa a 'false' los dados se detienen
      */
     private void tirarDados(){
         if(control) {
             int resID;
             String aux;
-            dado1 = (int) (Math.random() * 6);
-            dado2 = (int) (Math.random() * 6);
+            dado1 = (int) (Math.random()*6+1);
+            dado2 = (int) (Math.random()*6+1);
 
-            aux = arrDado[dado1];
+            aux = arrDado[dado1-1];
             resID = getResources().getIdentifier(aux, "drawable", getPackageName());
             img1.setImageResource(resID);
             img1.setVisibility(View.VISIBLE);
 
-            aux = arrDado[dado2];
+            aux = arrDado[dado2-1];
             resID = getResources().getIdentifier(aux, "drawable", getPackageName());
             img2.setImageResource(resID);
             img2.setVisibility(View.VISIBLE);
@@ -64,5 +87,76 @@ public class crapsPlay extends AppCompatActivity {
                 }
             }, 100);
         }
+    }
+
+    /**
+     * Simula la primera ronda del juego
+     * Ganas con 7 o 11 (10 monedas)
+     * Pierdes con 2, 3 o 12 (8 monedas)
+     * @return El numero que ha salido o 0 si ya ha finalizado
+     */
+    private int primeraRonda() {
+        tirarDados();
+        int total=dado1+dado2;
+        switch (total) {
+            case 7:
+            case 11:
+                // Ganas
+                estadoInicial();
+                Toast.makeText(this, "+"+MONEDAS_GANADAS+ " "+R.string.toastMoneda,
+                        Toast.LENGTH_SHORT).show();
+                monedas += MONEDAS_GANADAS;
+                break;
+            case 2:
+            case 3:
+            case 12:
+                // Pierdes
+                estadoInicial();
+                if (monedas <= 8) {
+                    monedas = 0;
+                } else {
+                    monedas -= MONEDAS_PERDIDAS;
+                }
+                Toast.makeText(this, "-"+MONEDAS_PERDIDAS+" "+R.string.toastMoneda,
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return total;
+    }
+
+    /**
+     * Simula la segunda ronda y las consecutivas en caso de que se den.
+     * Si se saca un 7 se pierde
+     * Si se saca el mismo numero que en la ronda 1 se gana
+     */
+    private void rondas() {
+        tirarDados();
+        if (valorTirada1 == (dado1+dado2)) {
+            // Ganas
+            estadoInicial();
+            Toast.makeText(this, "+"+MONEDAS_GANADAS+ " "+R.string.toastMoneda,
+                    Toast.LENGTH_SHORT).show();
+            monedas += MONEDAS_GANADAS;
+        }
+        if((dado1+dado2)==7){
+            // Pierdes
+            estadoInicial();
+            if (monedas <= 8) {
+                monedas = 0;
+            } else {
+                monedas -= MONEDAS_PERDIDAS;
+            }
+            Toast.makeText(this, "-"+MONEDAS_PERDIDAS+" "+R.string.toastMoneda,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Resea al valor inicial
+     */
+    private void estadoInicial(){
+        control=false;
+        hasJugado=false;
+        tiradaText.setText("");
     }
 }
