cafa9871d406b1386c2083dc9511b1f7fe096d80
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2017-11-17 15:10:20 +0100

    Añadidos_3
    -Dados funcionando, muy simple

--- app/src/main/java/com/example/cegoc/craps/crapsPlay.java
+++ app/src/main/java/com/example/cegoc/craps/crapsPlay.java
@@ -1,25 +1,76 @@
 package com.example.cegoc.craps;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class crapsPlay extends AppCompatActivity {
 
-    private int dado1,dado2;
+    private String arrDado[];
+    private ImageView img1;
+    private ImageView img2;
+    LinearLayout dadosLayout;
+    private Button roll;
+    private boolean control;
+    private int dado1, dado2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.craps_play);
+
+        control=false;
+        arrDado=getResources().getStringArray(R.array.dadosGris);
+        img1=(ImageView) findViewById(R.id.dado1);
+        img2=(ImageView) findViewById(R.id.dado2);
+        roll=(Button) findViewById(R.id.tirar);
+        dadosLayout=(LinearLayout)findViewById(R.id.dados);
+
+        dadosLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                control=!control;
+                if(control){
+                    roll.setText(getResources().getString(R.string.botonTirarStop));
+                } else{
+                    roll.setText(getResources().getString(R.string.botonTirar));
+                }
+                tirarDados();
+            }
+        });
     }
 
     /**
      * Se simula que se tiran 2 dados
      * @return
      */
-    private int tirarDados(){
+    private void tirarDados(){
+        if(control) {
+            int resID;
+            String aux;
+            dado1 = (int) (Math.random() * 6);
+            dado2 = (int) (Math.random() * 6);
 
+            aux = arrDado[dado1];
+            resID = getResources().getIdentifier(aux, "drawable", getPackageName());
+            img1.setImageResource(resID);
+            img1.setVisibility(View.VISIBLE);
 
-        return -1;
+            aux = arrDado[dado2];
+            resID = getResources().getIdentifier(aux, "drawable", getPackageName());
+            img2.setImageResource(resID);
+            img2.setVisibility(View.VISIBLE);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tirarDados();
+                }
+            }, 100);
+        }
     }
 }
